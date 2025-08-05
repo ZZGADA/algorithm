1e35bc85343c15f932cab16c51d8ac5de0c84cf5
zhangzan <zhangzan@tal.com>
2025-08-05 09:25:20 +0800

    有效括号

--- algorithm_go/inorderTraversal.go
+++ algorithm_go/inorderTraversal.go
@@ -1,5 +1,9 @@
 package algorithm_go
 
+import (
+	"strings"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -28,7 +32,39 @@ func inorderTraversal(root *TreeNode) []int {
 	return res
 }
 
-// flatten 将二叉树展开为链表`
-func flatten(root *TreeNode) {
+// generateParenthesis 生成有效括号
+func generateParenthesis(n int) []string {
+	// 回溯法
+	// 右括号在递归的过程中 数量一定比左括号少
+	sb := make([]string, 0)
+	res := make([]string, 0)
+	var dfs func(leftCnt int, rightCnt int)
 
+	dfs = func(leftCnt int, rightCnt int) {
+
+		// 括号数刚刚好一致的时候
+		if leftCnt == n && leftCnt == rightCnt {
+
+			res = append(res, strings.Join(sb, ""))
+		}
+
+		if leftCnt < n {
+			sb = append(sb, "(")
+
+			// 递归 然后回溯
+			dfs(leftCnt+1, rightCnt)
+			sb = sb[:len(sb)-1]
+		}
+
+		// 右边的括号数 必须小于左括号数
+		if rightCnt < leftCnt {
+			sb = append(sb, ")")
+
+			dfs(leftCnt, rightCnt+1)
+			sb = sb[:len(sb)-1]
+		}
+	}
+	dfs(0, 0)
+
+	return res
 }
